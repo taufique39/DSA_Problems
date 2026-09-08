e8c9ca78704aa410b0b4119a3c6d383bb9a54d9a
taufique39 <razataufique59@gmail.com>
2026-09-08 16:40:30 +0530

    Time: 7 ms (78.08%), Space: 43.7 MB (74.08%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+
+        int[] lasts = new int[256];
+        int[] lastt = new int[256];
+
+        for (int i = 0; i < s.length(); i++) {
+            if (lasts[s.charAt(i)] != lastt[t.charAt(i)]) {
+                return false;
+            }
+
+            lasts[s.charAt(i)] = i + 1;
+            lastt[t.charAt(i)] = i + 1;
+        }
+        return true;
+    }
+}
