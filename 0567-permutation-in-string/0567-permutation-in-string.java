48f2c90b71574e95af24fd3cebd082f2bc861c41
taufique39 <razataufique59@gmail.com>
2026-09-17 00:33:52 +0530

    Time: 6 ms (87.39%), Space: 44.3 MB (37.88%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -7,21 +7,58 @@ class Solution {
             return false;
         }
 
-        char[] x = s1.toCharArray();
-        Arrays.sort(x);
-        s1 = new String(x);
+        int[] s1_freq = new int[26];
+        int[] s2_freq = new int[26];
 
-        for (int i = 0; i <= b - a; i++) {
-            String substring = s2.substring(i, i + a);
+        for (char ch : s1.toCharArray()) {
+            s1_freq[ch - 'a']++;
+        }
 
-            char[] y = substring.toCharArray();
-            Arrays.sort(y);
-            substring = new String(y);
+        int i = 0;
+        int j = 0;
 
-            if (s1.equals(substring)) {
+        while (j < b) {
+            s2_freq[s2.charAt(j) - 'a']++;
+
+            if (j - i + 1 > a) {
+                s2_freq[s2.charAt(i) - 'a']--;
+                i++;
+            }
+
+            if (Arrays.equals(s1_freq, s2_freq)) {
                 return true;
             }
+
+            j++;
         }
+
         return false;
     }
 }
+// class Solution {
+//     public boolean checkInclusion(String s1, String s2) {
+//         int a = s1.length();
+//         int b = s2.length();
+
+//         if (a > b) {
+//             return false;
+//         }
+
+//         char[] x = s1.toCharArray();
+//         Arrays.sort(x);
+//         s1 = new String(x);
+
+//         for (int i = 0; i <= b - a; i++) {
+//             String substring = s2.substring(i, i + a);
+
+//             char[] y = substring.toCharArray();
+//             Arrays.sort(y);
+//             substring = new String(y);
+
+//             if (s1.equals(substring)) {
+//                 return true;
+//             }
+//         }
+//         return false;
+//     }
+// }
