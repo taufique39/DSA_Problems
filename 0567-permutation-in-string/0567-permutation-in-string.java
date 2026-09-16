c3ec9cabfe36dc3292dd77e10f6f8c7dccae912a
taufique39 <razataufique59@gmail.com>
2026-09-17 00:34:42 +0530

    Time: 8 ms (49.19%), Space: 44.3 MB (32.54%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,38 +1,62 @@
+// class Solution {
+//     public boolean checkInclusion(String s1, String s2) {
+//         int a = s1.length();
+//         int b = s2.length();
+
+//         if (a > b) {
+//             return false;
+//         }
+
+//         int[] s1_freq = new int[26];
+//         int[] s2_freq = new int[26];
+
+//         for (char ch : s1.toCharArray()) {
+//             s1_freq[ch - 'a']++;
+//         }
+
+//         int i = 0;
+//         int j = 0;
+
+//         while (j < b) {
+//             s2_freq[s2.charAt(j) - 'a']++;
+
+//             if (j - i + 1 > a) {
+//                 s2_freq[s2.charAt(i) - 'a']--;
+//                 i++;
+//             }
+
+//             if (Arrays.equals(s1_freq, s2_freq)) {
+//                 return true;
+//             }
+
+//             j++;
+//         }
+
+//         return false;
+//     }
+// }
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        int a = s1.length();
-        int b = s2.length();
-
-        if (a > b) {
+        if (s1.length() > s2.length())
             return false;
+
+        int[] a = new int[26];
+        int[] b = new int[26];
+
+        for (int i = 0; i < s1.length(); i++) {
+            a[s1.charAt(i) - 'a']++;
+            b[s2.charAt(i) - 'a']++;
         }
 
-        int[] s1_freq = new int[26];
-        int[] s2_freq = new int[26];
-
-        for (char ch : s1.toCharArray()) {
-            s1_freq[ch - 'a']++;
-        }
-
-        int i = 0;
-        int j = 0;
-
-        while (j < b) {
-            s2_freq[s2.charAt(j) - 'a']++;
-
-            if (j - i + 1 > a) {
-                s2_freq[s2.charAt(i) - 'a']--;
-                i++;
-            }
-
-            if (Arrays.equals(s1_freq, s2_freq)) {
+        for (int i = s1.length(); i < s2.length(); i++) {
+            if (Arrays.equals(a, b))
                 return true;
-            }
 
-            j++;
+            b[s2.charAt(i) - 'a']++;
+            b[s2.charAt(i - s1.length()) - 'a']--;
         }
 
-        return false;
+        return Arrays.equals(a, b);
     }
 }
 // class Solution {
